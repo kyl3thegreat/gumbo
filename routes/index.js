46edae4858822eb94f93c247a7c8fd9166f99a58
Jditieri <jonnyrzone420@yahoo.com>
2018-06-02 12:09:23 -0400

    pushprofile

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
 module.exports = router;
