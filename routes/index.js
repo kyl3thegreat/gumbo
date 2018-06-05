02215f99c10d4c144442382d421e7d501ce89683
Leslie Hernandez <lesliehernandez@Leslies-MacBook-Pro.local>
2018-06-05 00:41:36 -0400

    changes to profile page

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-
-/* GET settings page. */
-router.get('/settings', function(req, res, next) {
-  res.render('settings');
-});
-
-module.exports = router;
-
 /* GET settings page. */
 router.get('/settings', function(req, res, next) {
   res.render('settings');
@@ -25,6 +17,42 @@ module.exports = router;
 /* GET dashboard page. */
 router.get('/dashboard', function(req, res, next) {
   res.render('dashboard');
+});
+
+module.exports = router;
+
+/* GET history page. */
+router.get('/history', function(req, res, next) {
+  res.render('history');
+});
+
+module.exports = router;
+
+
+/* GET location page. */
+router.get('/location', function(req, res, next) {
+  res.render('location');
+});
+
+module.exports = router;
+
+/* GET matches page. */
+router.get('/matches', function(req, res, next) {
+  res.render('matches');
+});
+
+module.exports = router;
+
+/* GET profile page. */
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+module.exports = router;
+
+/* GET questionnaire page. */
+router.get('/questionnaire', function(req, res, next) {
+  res.render('questionnaire');
 });
 
 module.exports = router;
