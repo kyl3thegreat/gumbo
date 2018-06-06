428fb946e6788952f5604814d3dab5800847d1e4
Timothy Swengros <timswengros@Timothys-MacBook-Pro.local>
2018-06-06 17:10:16 -0400

    Profile edit form

--- public/javascripts/settingseditform.js
+++ public/javascripts/settingseditform.js
@@ -5,15 +5,19 @@
 
 
 
+
    $("#editsettings").on("click", ()=>{
+        $(this).val('Save');
         console.log("clicked")
         $('#settingsForm')
 .append('<form id="mySearch"></form>'); //append a new form element with id mySearch to <body>
-$('#mySearch') 
+$('#mySearch')
 //add in all the needed input elements
-.append('<Label>Email Address</Label><input class="editinput" type="text" name="btnSearch" id="btnSearch" value="">')
-.append('<Label>Age</Label><input class="editinput" type="text" name="txtField1" id="txtField" value="">')
-.append('<Label>Bio</Label><textField class="editinput" type="text" name="txtField2" id="txtField" value=""></textField>')
-.append('<Label>Gender</Label><input class="editinput" type="text" name="txtField3" id="selType" value="">')
-.append('<Label>Picture</Label><input class="editinput" type="file" name="txtField4" id="selType2" value="">')
+.append('<Label>Email Address</Label><input type="text" name="txtField" id="txtField" value="">')
+.append('<Label>Age</Label><input type="text" name="txtField1" id="txtField" value="">')
+.append('<Label>Bio</Label><textarea class="form-control" rows="3"></textarea> <br>')
+.append('<Label>Gender</Label><select><option value="Male">Male</option><option value="Female">Female</option><option value="Other">Other</option></select> <br><br>')
+.append('<Label>Picture</Label><input type="file" name="txtField5" id="txtField" value="">')
    })
+        
+    
